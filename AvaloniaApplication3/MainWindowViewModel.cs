71a6fb1714c36c8b4a8aae16893ff97332e064a6
Starinin Andrey <starininandrey@gmail.com>
2023-08-27 08:38:47 +0300

    Initial commit

---
+++ AvaloniaApplication3/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AvaloniaApplication3;
+
+public enum State {Default, State1, State2}
+
+public class MainWindowViewModel : BaseViewModel
+{
+    private State _state;
+    public State State
+    {
+        get => _state; 
+        set => SetField(ref _state, value);
+    }
+}
