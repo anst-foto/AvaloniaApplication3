494a6d8071ad3693a5c5f0b135e9cf012429ad75
Starinin Andrey <starininandrey@gmail.com>
2023-09-03 07:26:15 +0300

    Updated

--- AvaloniaApplication3/MainWindowViewModel.cs
+++ AvaloniaApplication3/MainWindowViewModel.cs
@@ -1,13 +1,32 @@
 ﻿namespace AvaloniaApplication3;
 
-public enum State {Default, State1, State2}
+//public enum State {Default, State1, State2}
 
 public class MainWindowViewModel : BaseViewModel
 {
-    private State _state;
+    /*private State _state;
     public State State
     {
         get => _state; 
         set => SetField(ref _state, value);
+    }*/
+
+    private bool _isUpdated;
+    
+    public bool IsUpdated
+    {
+        get => _isUpdated;
+        set => SetField(ref _isUpdated, value);
+    }
+
+    private string _text;
+    public string Text
+    {
+        get => _text;
+        set
+        {
+            SetField(ref _text, value);
+            IsUpdated = _text == "12345";
+        }
     }
 }
